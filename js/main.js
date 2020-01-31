b1893942ae633c5e6bed20be235e6b213e189b12
JonathanDutt <Jonathan.dutt@georgebrown.ca>
2020-01-31 09:11:46 -0500

    UI changes done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+// ************ STORE ALL DOM REFERENCES *******
+let $modal = document.querySelector('.modal')
+let $user = document.querySelector('#user')
+let $pass = document.querySelector('#pass')
+let $signin = document.querySelector('.signin')
+let $close = document.querySelector('.close')
+let $click = document.querySelector('.click')
 
+
+// *********** UI CHANGES ***********
 // Change the display style to block for the .model
 // Change the display style to none for the .model 
 // add .error class to #user
@@ -6,6 +15,7 @@
 // Remove .error class from #user
 // Remove .error class from #pass
 
+// ******** EVENT LISTENERS *********
 // Capture .signin click
 // Capture .close click
 // Capture .submit click

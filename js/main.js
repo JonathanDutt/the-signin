5ce2fd581857bbe939aa95d8436affa8bca5a35c
JonathanDutt <Jonathan.dutt@georgebrown.ca>
2020-01-31 08:45:43 -0500

    Comment based proto

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Capture .signin click
+
+// Change the display style to block for the .model
+
+// Capture .close click
+
+// Capture Change the display style to none for the .model 
+
+// Capture .submit click
+
+// add .error class to #user
+
+// add .error class to #pass
+
+// Capture #user fpcis 
+
+// Remove .error class from #user
+
+// Capture #pass focus 
+
+// Remove .error class from #pass
